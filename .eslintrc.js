259e558f48ff96bb95ea7755c7dcd2ed5efa2313
“sundung” <sun1358742@163.com>
2022-05-03 01:06:48 +0800

    集成 prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,16 +6,18 @@ module.exports = {
         ecmaVersion: 2020,
         sourceType: 'module',
         ecmaFeatures: {
-            jsx: true
-        }
+            jsx: true,
+        },
     },
 
     extends: [
         'plugin:vue/vue3-recommended',
         'plugin:@typescript-eslint/recommended',
+        'prettier',
+        'plugin:prettier/recommended',
     ],
 
     rules: {
         // override/add rules settings here, such as:
-    }
+    },
 };
