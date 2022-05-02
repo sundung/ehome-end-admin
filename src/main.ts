95eb9fa4e33b644821ae11e95ebd2743ccd4d027
“sundung” <sun1358742@163.com>
2022-05-03 01:11:40 +0800

    集成 pinia

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,6 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-
-createApp(App).mount('#app')
+import { createApp } from 'vue';
+import App from './App.vue';
+import store from './store'; // 创建vue实例
+const app = createApp(App); // 挂载pinia
+app.use(store); // 挂载实例
+app.mount('#app');
