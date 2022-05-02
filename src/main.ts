5d721395f492c77fdbb85011ca25c12e1add7070
“sundung” <sun1358742@163.com>
2022-05-03 01:15:07 +0800

    集成 vue-router4

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import { createApp } from 'vue';
 import App from './App.vue';
 import store from './store'; // 创建vue实例
+import router from '@/router';
 const app = createApp(App); // 挂载pinia
-app.use(store); // 挂载实例
+app.use(store); // 挂载状态管理器
+app.use(router); // 挂载路由
 app.mount('#app');
